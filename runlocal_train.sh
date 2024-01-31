588f5b4ef6c63aa044c42cd332e8cb6043c6440e
seaskyho <seaskyho@gmail.com>
2024-01-31 20:56:48 +1100

    update

--- runlocal_train.sh
+++ runlocal_train.sh
@@ -2,7 +2,7 @@ python -u efficientad.py \
   --dataset mvtec_loco \
   --subdataset breakfast_box \
   --imagenet_train_path ./datasets/Imagenet/ILSVRC/Data/CLS-LOC/train \
-  --analysis_heatmap \
+  
   --pretrained_network mae_vit \
   --mae_vit_teacher \
   --image_size_mae_vit_teacher 512 \
